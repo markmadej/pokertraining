44cdb7f6a6dfeb2dd5c53037278d721975794520
Madej <marmadej@a45e60e22153.ant.amazon.com>
2016-03-23 13:26:47 -0700

    Little work on the hand range, added empty test class

--- src/main/java/domain/HoleCardRange.java
+++ src/main/java/domain/HoleCardRange.java
@@ -19,4 +19,37 @@ public class HoleCardRange {
 		
 	}
 	
+	/**
+	 * This constructor takes a human readable range.  The range follows these rules:
+	 * 
+	 * Two of the same card (22, 33, etc) is a pair.  A plus sign after it means
+	 *   every pair higher than that pair.
+	 * A card followed by a suit character (c/d/h/s) means the card is that suit.  "x"
+	 *   means any suit.  If there are two cards followed by a "s" (and the first card
+	 *   is not followed by a suit) then the "s" means "suited".  
+	 * AJ means any ace/jack combination.  AJs means all 4 suited combinations.
+	 * AcJs means ace of clubs, jack of spades.  AxJs means any ace with the jack of spades.
+	 * QJ+ means any queen/jack combination as well as any combination of higher cards.  
+	 *   So QJ+ means: QJ, KJ, AJ, AQ, and AK.
+	 * Finally, different ranges can be separated by commas.  For example : 22+,AQ+
+	 *   
+	 * @param range
+	 */
+	public HoleCardRange(String range) {
+		range.replace(" ", "");  //Remove spaces
+		range = range.toUpperCase();  //Simplify our logic by knowing everything is uppercase
+		String[] ranges = range.split(",");
+		for (String rng : ranges) {
+			parseRange(rng);
+		}
+	}
+	
+	private void parseRange(String range) {
+		
+	}
+	
+	private boolean isPlus(String range) {
+		return range.endsWith("+");
+	}
+	
 }
---
+++ src/test/java/domain/HoleCardRangeTest.java
@@ -0,0 +1,14 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HoleCardRangeTest {
+
+	@Test
+	public void testPlusCheck() {
+		//Test whether the internal plus check method works
+	}
+
+}
