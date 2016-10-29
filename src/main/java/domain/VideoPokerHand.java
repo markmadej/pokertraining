2db67dee02875d9a80e054cdf5cf8e1ecb5209f5
mark <mark@169.254.0.3>
2016-10-28 18:13:50 -0700

    All tests pass.  Time to add some more tests to make sure all edge cases are caught.

--- src/main/java/domain/VideoPokerHand.java
+++ src/main/java/domain/VideoPokerHand.java
@@ -17,7 +17,8 @@ public class VideoPokerHand {
 	private ArrayList<Card> sortedCards = new ArrayList<Card>(5);
 	
 	public VideoPokerHand(ArrayList<Card> cards) {
-		calculateHand(cards);
+		normalizeCards(cards);
+		normalizedHand = createNormalizedString(sortedCards);
 	}
 
 	/*
@@ -33,12 +34,15 @@ public class VideoPokerHand {
 	 * Within each suit, arrange from the highest card to lowest, left to right
 	 * Change suits to suit 1, 2, 3 (ie AcJcAdJd5s would translate to A1J1A2J253)
 	 */
-	private void calculateHand(ArrayList<Card> cards) {
+	private void normalizeCards(ArrayList<Card> cards) {
 		int[] cardsPerSuit = new int[5]; // Need 5 since suits are currently 1-indexed
 		Arrays.fill(cardsPerSuit, 0);
 		sortedCards.clear(); 
-		originalCards = (Card[]) cards.toArray();
+		originalCards = new Card[5];
 		
+		for (int i = 0; i < cards.size(); i++) {
+			originalCards[i] = cards.get(i);
+		}
 		//Create counters for each suit that give you the number of cards in each suit, which will be used for sorting.
 		for (int i = 0; i < 5; i++) {
 			Card c = originalCards[i];
@@ -47,7 +51,7 @@ public class VideoPokerHand {
 		
 		sortedCards.add(0, originalCards[0]);
 		for (int i = 1; i < 5; i++) {
-			// Go through each card in the sorted array and see where the new one should go.
+
 			Card newCardToInsert = originalCards[i];
 			boolean insertedCard = false;
 			for (int j = 0; j < sortedCards.size(); j++) {
@@ -66,22 +70,61 @@ public class VideoPokerHand {
 					}
 				}
 				
-				if (cardsPerSuit[newCardToInsert.getSuit()] <= cardsPerSuit[compareCard.getSuit()]) {
-					//This card (and all others in its suit) needs to be placed after this card's suit.
-					//When the suits are equal this card could be placed before or after.  We'll do another pass to resolve that.
-					continue;
-				}
-				
 				if (cardsPerSuit[newCardToInsert.getSuit()] > cardsPerSuit[compareCard.getSuit()]) {
-					// If this card's suit has more than the current compare card, bump the compare card over one and insert.
+					// If this card's suit has more than (or the same as) the current compare card, bump the compare card over one and insert.
 					sortedCards.add(j, newCardToInsert);
 					insertedCard = true;
 					break;
+				}	
+				if (cardsPerSuit[newCardToInsert.getSuit()] == 1 && cardsPerSuit[compareCard.getSuit()] == 1) {
+					if (newCardToInsert.getDenomination() > compareCard.getDenomination()) {
+						sortedCards.add(j, newCardToInsert);
+						insertedCard = true;
+						break;
+					} else {
+						continue;
+					}
 				}
+			}
+			if (!insertedCard) {
+				sortedCards.add(sortedCards.size(), newCardToInsert);
+			}
 
-				
+		}
+		
+		// Now sortedCards has the cards in order, but there is one final potential issue.  If there are two pairs of two suits, 
+		// those may be out of order.  Let's see if that is the case and fix it.
+		if (sortedCards.get(0).getSuit() == sortedCards.get(1).getSuit() && sortedCards.get(2).getSuit() == sortedCards.get(3).getSuit()) {
+			if ((sortedCards.get(2).getDenomination() > sortedCards.get(0).getDenomination()) ||
+					(sortedCards.get(2).getDenomination() == sortedCards.get(0).getDenomination() &&
+							sortedCards.get(3).getDenomination() > sortedCards.get(1).getDenomination()
+							)
+					) {
+				// We need to flip the first two cards with the second two cards.
+				Card tmp1 = sortedCards.get(0);
+				Card tmp2 = sortedCards.get(1);
+				Card c3 = sortedCards.remove(2);
+				Card c4 = sortedCards.remove(2);
+				sortedCards.add(2, tmp2);
+				sortedCards.add(2, tmp1);
 			}
 		}
+	}
+
+	public String createNormalizedString(ArrayList<Card> normalizeCards) {
+		// Create the normalized string.
+		StringBuffer nb = new StringBuffer();
+		int lastSuitNum = 0;
+		int lastSuit = -1;
+		for (Card c : normalizeCards) {
+			nb.append(c.shortDenominationString());
+			if (lastSuit != c.getSuit()) {
+				lastSuitNum++;
+				lastSuit = c.getSuit();
+			}
+			nb.append(lastSuitNum);
+		}
+		return nb.toString();
 	}
 
 
@@ -89,5 +132,11 @@ public class VideoPokerHand {
 	
 	public String getNormalizedHand() {
 		return normalizedHand;
+	}
+
+	public ArrayList<Card> getSortedCards() {
+		// This was created solely for use within unit tests.
+		// No guarantees on this method's behavior are given if this is applied to any other use cases.
+		return sortedCards;
 	}
 }
--- src/test/java/domain/VideoPokerHandTest.java
+++ src/test/java/domain/VideoPokerHandTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class VideoPokerHandTest {
 
 	@Test
-	public void testNormalize1() {
+	public void testSort1AlreadySorted() {
 		// Test hand : AcJcAdJd5s
 		// Rearranged : AcJcAdJc5s (same)
 		// Result with numbers : A1J1A2J253
@@ -22,28 +22,85 @@ public class VideoPokerHandTest {
 		cards.add(new Card(Card.SPADES, Card.FIVE));
 
 		VideoPokerHand vph = new VideoPokerHand(cards);
-		String normalizedHand = vph.getNormalizedHand();
-		String expectedValue = "A1J1A2J253";
-		assertTrue(normalizedHand + " did not match the expected value of " + expectedValue, normalizedHand.equals(expectedValue));
+		
+		ArrayList<Card> sortedCards = vph.getSortedCards();
+		assertTrue("5 cards were not found after sorting.", sortedCards.size() == 5);
+		for (int i = 0; i < 5; i++) {
+			assertTrue("Cards did not match at position " + i + "\ncards:" + cards + "\nsortedCards:" + sortedCards, 
+					cards.get(i).getSuit() == sortedCards.get(i).getSuit() && 
+					cards.get(i).getDenomination() == sortedCards.get(i).getDenomination());
+		}
+	}
+	
+	@Test
+	public void testNormalize1() {
+		// Test hand : AcJcAdJd5s
+		// Rearranged : AcJcAdJc5s (same)
+		// Result with numbers : A1J1A2J253
+		
+		ArrayList<Card> cards = new ArrayList<Card>();
+		cards.add(new Card(Card.CLUBS, Card.ACE));
+		cards.add(new Card(Card.CLUBS, Card.JACK));
+		cards.add(new Card(Card.DIAMONDS, Card.ACE));
+		cards.add(new Card(Card.DIAMONDS, Card.JACK));
+		cards.add(new Card(Card.SPADES, Card.FIVE));
+
+		VideoPokerHand vph = new VideoPokerHand(cards); //The cards passed in here are irrelevant - just needed a constructor to use
+		String normalizedHand = vph.createNormalizedString(cards);
+		
+		assertTrue(normalizedHand + " did not match expected value of A1J1A2J253.", 
+				normalizedHand.equals("A1J1A2J253"));
+	}
+	
+	@Test
+	public void testSort2() {
+		// Test hand : 7s2s5h4c8d
+		// Rearranged : 7s2s8d5h4c
+		// Result with numbers : 7121825344
+		
+		ArrayList<Card> cards = new ArrayList<Card>();
+		cards.add(new Card(Card.SPADES, Card.SEVEN));
+		cards.add(new Card(Card.SPADES, Card.DEUCE));
+		cards.add(new Card(Card.HEARTS, Card.FIVE));
+		cards.add(new Card(Card.CLUBS, Card.FOUR));
+		cards.add(new Card(Card.DIAMONDS, Card.EIGHT));
+
+		VideoPokerHand vph = new VideoPokerHand(cards);
+		
+		ArrayList<Card> expectedCards = new ArrayList<Card>();
+		expectedCards.add(new Card(Card.SPADES, Card.SEVEN));
+		expectedCards.add(new Card(Card.SPADES, Card.DEUCE));
+		expectedCards.add(new Card(Card.DIAMONDS, Card.EIGHT));
+		expectedCards.add(new Card(Card.HEARTS, Card.FIVE));
+		expectedCards.add(new Card(Card.CLUBS, Card.FOUR));
+		
+		ArrayList<Card> sortedCards = vph.getSortedCards();
+		assertTrue("5 cards were not found after sorting.", sortedCards.size() == 5);
+		for (int i = 0; i < 5; i++) {
+			assertTrue("Cards did not match at position " + i + "\nexpectedCards:" + expectedCards + "\nsortedCards:" + sortedCards, 
+					expectedCards.get(i).getSuit() == sortedCards.get(i).getSuit() && 
+					expectedCards.get(i).getDenomination() == sortedCards.get(i).getDenomination());
+		}
 	}
 	
 	@Test
 	public void testNormalize2() {
-		// Test hand : Ac5d7d2s2c
-		// Rearranged : Ac2c7d5d2s
-		// Result with numbers : A121725223
+		// Test hand : 7s2s5h4c8d
+		// Rearranged : 7s2s8d5h4c
+		// Result with numbers : 7121825344
 		
 		ArrayList<Card> cards = new ArrayList<Card>();
-		cards.add(new Card(Card.CLUBS, Card.ACE));
-		cards.add(new Card(Card.DIAMONDS, Card.FIVE));
-		cards.add(new Card(Card.DIAMONDS, Card.SEVEN));
+		cards.add(new Card(Card.SPADES, Card.SEVEN));
 		cards.add(new Card(Card.SPADES, Card.DEUCE));
-		cards.add(new Card(Card.CLUBS, Card.DEUCE));
+		cards.add(new Card(Card.DIAMONDS, Card.EIGHT));
+		cards.add(new Card(Card.HEARTS, Card.FIVE));
+		cards.add(new Card(Card.CLUBS, Card.FOUR));
 
-		VideoPokerHand vph = new VideoPokerHand(cards);
-		String normalizedHand = vph.getNormalizedHand();
-		String expectedValue = "A121725223";
-		assertTrue(normalizedHand + " did not match the expected value of " + expectedValue, normalizedHand.equals(expectedValue));
+		VideoPokerHand vph = new VideoPokerHand(cards); //The cards passed in here are irrelevant - just needed a constructor to use
+		String normalizedHand = vph.createNormalizedString(cards);
+		
+		assertTrue(normalizedHand + " did not match expected value of 7121825344.", 
+				normalizedHand.equals("7121825344"));
 	}
 
 }
